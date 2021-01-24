cdd94591eeb84409f08b2745ee13373ab43cc0aa
Levent ANT <leventantt@gmail.com>
2021-01-25 00:34:35 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // type safety - tip güvenliği
+           // Do not repaet yourself - Kendini tekrarlama
+            
+            string kategoriEtiketi = "kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGrisYapmisMi = true;
+
+            if (sistemeGrisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+        }
+    }
+}
